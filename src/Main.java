eb3e749a0db5f6024dd0bb832ee1b84e456bcf13
Forbesy <mvforbes21@gmail.com>
2023-06-19 16:20:27 -0400

    Updated

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,35 @@
+import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Scanner in = new Scanner(System.in);
+
+        double meters;
+        boolean inputValid;
+
+        do {
+            System.out.println("Enter a measurement in meters: ");
+            if (!in.hasNextDouble()) {
+                System.out.println("Invalid. Please enter a valid number.");
+                in.nextLine();
+                inputValid = false;
+            } else {
+                meters = in.nextDouble();
+                if (meters < 0) {
+                    System.out.println("Invalid. Please enter a positive number.");
+                    inputValid = false;
+                } else {
+                    inputValid = true;
+
+                    double miles = meters * 0.000621371;
+                    double feet = meters * 3.28084;
+                    double inches = meters * 39.3701;
+
+                    System.out.println("Miles: " + miles);
+                    System.out.println("Feet: " + feet);
+                    System.out.println("Inches: " + inches);
+                }
+            }
+        }
+        while (!inputValid);
     }
 }
